26ccb1ce8d0c9399b1896ee2282fed7c54d977a2
Md Mahamudul <mdmahamudulkarim5@gmail.com>
2026-09-25 15:42:18 +0600

    fix login and netlify routing

--- src/service/BaseUrl.jsx
+++ src/service/BaseUrl.jsx
@@ -1,3 +1,1 @@
-// export default "https://library-management-fast-api.onrender.com";
- //export default "https://housing-roommate-management-system.onrender.com";
-export default "http://127.0.0.1:8000";
+export default "https://housing-roommate-management-system.onrender.com";
